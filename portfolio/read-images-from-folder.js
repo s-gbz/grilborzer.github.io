cf7d84f8baa72a80b069fdca34b2c2e7333d2f11
Sergej Grilborzer <sergej1995@gmail.com>
2019-12-30 20:10:01 +0100

    Refactor Portfolio folder structure

--- portfolio/read-images-from-folder.js
+++ portfolio/read-images-from-folder.js
@@ -1,8 +1,10 @@
 let vue_data = {
-    fullImagePaths: [],
+    fullSizeImagePaths: [],
+    thumbnailImagePaths: [],
     index: null
 };
-const folder_path = "images/thumbs/";
+const fullSizeFolderPath = "images/fulls/";
+const thumbnailFolderPath = "images/thumbnails/";
 
 new Vue({
     delimiters: ["((", "))"],
@@ -15,13 +17,15 @@ new Vue({
 });
 
 $.ajax({
-    url: folder_path,
+    url: fullSizeFolderPath,
 
     success: function (data) {
         $(data).find("a").attr("href", function (i, read_img_path) {
             if (read_img_path.match(/\.(jpe?g)$/)) {
-                const full_image_path = folder_path + read_img_path;
-                vue_data.fullImagePaths.push(full_image_path);
+                const fullFullSizeImagePath = fullSizeFolderPath + read_img_path;
+                const fullThumbnailImagePath = thumbnailFolderPath + read_img_path;
+                vue_data.fullSizeImagePaths.push(fullFullSizeImagePath);
+                vue_data.thumbnailImagePaths.push(fullThumbnailImagePath);
             }
         });
     }
